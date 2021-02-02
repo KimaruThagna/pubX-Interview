f77a43370684caf3811b6524c862c4f11af229ed
Kimaru thagana <macbuntu@localhost.localdomain>
2021-02-02 17:00:10 +0300

    polish index.js

--- pubx/src/index.js
+++ pubx/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const root = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+      <App data={root.getAttribute('data-details')} />
+  </React.StrictMode>, root)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
